8a6628353dd692f0fac939fca1f8ccd9d659ff1b
RajeevSaiThota <64220936+RajeevSaiThota@users.noreply.github.com>
2021-06-07 12:49:28 +0530

    First Commit

---
+++ test/src/main/java/com/practise/test/Decrypt.java
@@ -0,0 +1,15 @@
+package com.practise.test;
+
+public class Decrypt {
+
+	public static void main(String[] args) {
+		
+		EncryptDecrypt decrypt = new EncryptDecrypt();
+		
+		System.out.println(decrypt.encrypt("fanniemae"));
+		
+		/*String decrypt1 = decrypt.decrypt("de3oocgK39jS0BrEuWOdWQ==");
+		System.out.println(decrypt1);*/
+	}
+
+}
---
+++ test/src/main/java/com/practise/test/Pattern.java
@@ -0,0 +1,17 @@
+package com.practise.test;
+
+public class Pattern {
+	
+
+	    public static void main(String[] args) {
+	        int rows = 9;
+
+	        for(int i = 1; i <= rows; ++i) {
+	            for(int j = 1; j <= i; ++j) {
+	                System.out.print(j + " ");
+	            }
+	            System.out.println();
+	        }
+	    }
+
+}
